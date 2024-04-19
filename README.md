38de07402ef0fc2c4d4f262322c693e81d50cddb
glauz <gabriel.lauzier@usherbrooke.ca>
2024-04-19 08:29:30 -0400

    Refactor Agent class

---
+++ README.md
@@ -0,0 +1,21 @@
+# Experiments with reinforcement learning
+
+## Quick start
+ ```Bash
+ python -m venv .venv
+
+ .\.venv\Scripts\Activate.ps1 # Powershell
+ .\.venv\Scripts\activate.bat # Windows cmd
+ source .venv/bin/activate # Ubuntu
+
+ python -m pip install --upgrade pip
+
+ pip install -r .\requirements.txt
+ ```
+
+ **Note:** You might need the following for `Powershell`:
+ ```Bash
+ Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
+ ```
+
+ **Note 2:** To use GPUs with PyTorch, you should download the required package according to your needs from https://pytorch.org/ and make sure to replace the version installed with the [requirements.txt](requirements.txt).
--- ppo/agent.py
+++ ppo/agent.py
@@ -7,6 +7,10 @@ import torch
 from .mlp import MLP
 
 class Agent(abc.ABC):
+
+    action_space: gymnasium.Space
+    observation_space: gymnasium.Space
+
     def __init__(self, env: gymnasium.Env, clip_coef=0.2, value_coef=0.5, entropy_coef=0.01):
         self.clip_coef = clip_coef
         self.value_coef = value_coef
@@ -15,53 +19,14 @@ class Agent(abc.ABC):
         self.observation_space_size = numpy.array(env.single_observation_space.shape).prod()
         self.action_space_size = numpy.array(env.single_action_space.shape).prod()
 
-    @abc.abstractmethod
-    def get_value(self, x):
-        pass
-
-    @abc.abstractmethod
-    def get_action_and_value(self, x, action=None):
-        pass
-
-
-class DiscreteAgent(Agent):
-    def __init__(self, env: gymnasium.Env, clip_coef=0.2, value_coef=0.5, entropy_coef=0.01):
-        super(DiscreteAgent, self).__init__(env, clip_coef, value_coef, entropy_coef)
-
-        self.actor = MLP(self.observation_space_size, self.action_space_size, 64, activation=torch.nn.Tanh)
-        self.critic = MLP(self.observation_space_size, 1, 64, activation=torch.nn.Tanh)
-
+        self.actor = self.create_actor()
+        self.critic = self.create_critic()
+        
     def get_value(self, x):
         return self.critic(x)
 
     def get_action_and_value(self, x, action=None):
-        action_logits = self.actor(x)
-        probs = torch.distributions.Categorical(logits=action_logits)
-
-        if action is None:
-            action = probs.sample()
-
-        return action, probs.log_prob(action), probs.entropy(), self.critic(x)
-
-
-class ContinuousAgent(Agent):
-    def __init__(self, env: gymnasium.Env, clip_coef=0.2, value_coef=0.5, entropy_coef=0.01):
-        super(ContinuousAgent, self).__init__(env, clip_coef, value_coef, entropy_coef)
-
-        self.actor = MLP(self.observation_space_size, self.action_space_size, 64, activation=torch.nn.Tanh)
-        self.critic = MLP(self.observation_space_size, 1, 64, activation=torch.nn.Tanh)
-
-        self.actor_std = torch.nn.Parameter(torch.zeros(1, self.action_space_size))
-
-    def get_value(self, x):
-        return self.critic(x)
-
-    def get_action_and_value(self, x, action=None):
-        action_mean = self.actor(x).reshape(-1, self.action_space_size)
-        action_log_std = self.actor_std.expand_as(action_mean)
-        action_std = torch.exp(action_log_std)
-
-        probs = torch.distributions.Normal(action_mean, action_std)
+        probs = self._get_action_distribution(x)
 
         if action is None:
             action = probs.sample()
@@ -70,3 +35,48 @@ class ContinuousAgent(Agent):
             action = action.squeeze(0)
 
         return action, probs.log_prob(action).sum(1), probs.entropy().sum(1), self.critic(x)
+
+    @abc.abstractmethod
+    def create_actor(self):
+        pass
+
+    @abc.abstractmethod
+    def create_critic(self):
+        pass
+
+    @abc.abstractmethod
+    def _get_action_distribution(self, x):
+        pass
+
+
+class DiscreteAgent(Agent):
+    def create_actor(self):
+        return MLP(self.observation_space_size, self.action_space_size, 64, activation=torch.nn.Tanh)
+
+    def create_critic(self):
+        return MLP(self.observation_space_size, 1, 64, activation=torch.nn.Tanh)
+    
+    def _get_action_distribution(self, x):
+        action_logits = self.actor(x)
+        return torch.distributions.Categorical(logits=action_logits)
+
+
+class ContinuousAgent(Agent):
+    def __init__(self, env: gymnasium.Env, clip_coef=0.2, value_coef=0.5, entropy_coef=0.01):
+        super(ContinuousAgent, self).__init__(env, clip_coef, value_coef, entropy_coef)
+
+        self.actor_std = torch.nn.Parameter(torch.zeros(1, self.action_space_size))
+
+    def create_actor(self):
+        return MLP(self.observation_space_size, self.action_space_size, 64, activation=torch.nn.Tanh)
+
+    def create_critic(self):
+        return MLP(self.observation_space_size, 1, 64, activation=torch.nn.Tanh)
+    
+    def _get_action_distribution(self, x):
+        action_mean = self.actor(x).reshape(-1, self.action_space_size)
+
+        action_log_std = self.actor_std.expand_as(action_mean)
+        action_std = torch.exp(action_log_std)
+
+        return torch.distributions.Normal(action_mean, action_std)
